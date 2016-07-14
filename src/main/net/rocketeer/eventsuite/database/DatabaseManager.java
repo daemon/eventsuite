d27bebcd9d084720856b859f471d4af1f26a7593
daemon <0x8BADC0DE@gmail.com>
2016-07-14 13:24:33 -0400

    Add SQL stream executor
    - executes SQL statements from InputStream
    - DELIMITER support for creating stored functions/procedures

--- src/main/net/rocketeer/eventsuite/database/DatabaseManager.java
+++ src/main/net/rocketeer/eventsuite/database/DatabaseManager.java
@@ -5,8 +5,9 @@ import net.rocketeer.eventsuite.ConfigManager;
 import net.rocketeer.eventsuite.EventSuitePlugin;
 
 import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
 public class DatabaseManager {
@@ -31,7 +32,6 @@ public class DatabaseManager {
     this.source.setMaxIdleTime(3600);
     this.source.setAutoCommitOnClose(true);
     this.source.setDriverClass("com.mysql.jdbc.Driver");
-
   }
 
   public Connection getConnection() throws SQLException
@@ -39,26 +39,10 @@ public class DatabaseManager {
     return this.source.getConnection();
   }
 
-  public void createDefaultTables() throws SQLException {
-    try (Connection c = this.getConnection()) {
-      c.createStatement().execute("CREATE TABLE IF NOT EXISTS arenas (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY " +
-          "NOT NULL, name VARCHAR(32) NOT NULL, map_base_pt_id INT NOT NULL, INDEX name_i(name), UNIQUE(name)) ENGINE=MyISAM");
-      c.createStatement().execute("CREATE TABLE IF NOT EXISTS regions (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY " +
-          "NOT NULL, name VARCHAR(32) NOT NULL, p1_id INT UNSIGNED NOT NULL, p2_id INT UNSIGNED NOT NULL, py_id INT UNSIGNED NOT NULL LINESTRING NOT NULL, SPATIAL INDEX " +
-          "xz_region_i(xz_region), SPATIAL INDEX y_range_i(y_range)) ENGINE=MyISAM");
-      c.createStatement().execute("CREATE TABLE IF NOT EXISTS points (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY " +
-          "NOT NULL, name VARCHAR(32) NOT NULL, xz POINT NOT NULL, y INT NOT NULL, z INT NOT NULL, server_id INT UNSIGNED NOT NULL, " +
-          "world_id INT UNSIGNED NOT NULL) ENGINE=MyISAM");
-      c.createStatement().execute("CREATE TABLE IF NOT EXISTS arena_region_assoc (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY " +
-          "NOT NULL, arena_id INT UNSIGNED NOT NULL, region_id INT UNSIGNED NOT NULL, INDEX arena_id_i(arena_id)) ENGINE=MyISAM");
-      c.createStatement().execute("CREATE TABLE IF NOT EXISTS arena_point_assoc (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY " +
-          "NOT NULL, arena_id INT UNSIGNED NOT NULL, point_id INT UNSIGNED NOT NULL, INDEX arena_id_i2(arena_id)) ENGINE=MyISAM");
-      c.createStatement().execute("CREATE TABLE IF NOT EXISTS servers (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY " +
-          "NOT NULL, name VARCHAR(32) NOT NULL) ENGINE=MyISAM");
-      c.createStatement().execute("CREATE TABLE IF NOT EXISTS worlds (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY " +
-          "NOT NULL, name VARCHAR(32) NOT NULL) ENGINE=MyISAM");
-      c.createStatement().execute("DROP FUNCTION IF EXISTS CreateEnvelope");
-      c.createStatement().execute("CREATE FUNCTION CreateEnvelope(p1 POINT, p2 POINT) RETURNS POLYGON DETERMINISTIC BEGIN DECLARE bounding POLYGON; SET bounding = Envelope(LineString(p1, p2)); RETURN bounding; END");
+  public void createDefaultTables() throws SQLException, IOException {
+    try (Connection c = this.getConnection();
+         InputStream in = this.getClass().getResourceAsStream("schema.sql")) {
+      new SqlStreamExecutor(c, in).execute();
     }
   }
 }
---
+++ src/main/net/rocketeer/eventsuite/database/SqlStreamExecutor.java
@@ -0,0 +1,114 @@
+package net.rocketeer.eventsuite.database;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.CharBuffer;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Stack;
+
+// Adds delimiter support for executing an SQL file
+public class SqlStreamExecutor {
+  private enum State { QUERY_BUILD, DELIMITER_CHANGE, DELIMITER_CHANGING, DELIMITER_CHECK }
+  private final Connection connection;
+  private final InputStreamReader in;
+  private String delimiter = ";";
+  private static final String DELIMITER_KEYWORD = "DELIMITER";
+  private Stack<StringBuilder> parseStack;
+  private State state = State.QUERY_BUILD;
+  private int delimCheckIndex = 0;
+  private int delimChangeIndex = 0;
+
+  public SqlStreamExecutor(Connection connection, InputStream stream) {
+    this.in = new InputStreamReader(stream);
+    this.connection = connection;
+  }
+
+  private void executeStatement(String statement) {
+    try {
+      this.connection.createStatement().execute(statement);
+    } catch (SQLException e) {}
+  }
+
+  private void doParse(char c) {
+    switch (this.state) {
+    case QUERY_BUILD:
+      if (c != this.delimiter.charAt(0) && c != 'D') {
+        this.parseStack.peek().append(c);
+        return;
+      }
+      this.parseStack.push(new StringBuilder());
+      this.parseStack.peek().append(c);
+      if (c == this.delimiter.charAt(0)) {
+        this.state = State.DELIMITER_CHECK;
+        ++this.delimCheckIndex;
+      } else {
+        this.state = State.DELIMITER_CHANGE;
+        ++this.delimChangeIndex;
+      }
+      break;
+    case DELIMITER_CHANGE:
+      if (this.delimCheckIndex >= DELIMITER_KEYWORD.length()) {
+        this.state = State.DELIMITER_CHANGING;
+        this.delimCheckIndex = 0;
+        this.parseStack.peek().setLength(0);
+      } else if (DELIMITER_KEYWORD.charAt(this.delimCheckIndex) == c) {
+        ++this.delimCheckIndex;
+        this.parseStack.peek().append(c);
+      } else {
+        String query = this.parseStack.pop().toString();
+        this.parseStack.peek().append(query);
+        this.state = State.QUERY_BUILD;
+        this.delimCheckIndex = 0;
+        this.doParse(c);
+      }
+      break;
+    case DELIMITER_CHECK:
+      if (this.delimChangeIndex >= this.delimiter.length()) {
+        this.state = State.QUERY_BUILD;
+        this.delimChangeIndex = 0;
+        this.parseStack.pop();
+        this.executeStatement(this.parseStack.pop().toString());
+        this.parseStack.push(new StringBuilder());
+        this.doParse(c);
+      } else if (DELIMITER_KEYWORD.charAt(this.delimChangeIndex) == c) {
+        ++this.delimChangeIndex;
+        this.parseStack.peek().append(c);
+      } else {
+        String query = this.parseStack.pop().toString();
+        this.parseStack.peek().append(query);
+        this.state = State.QUERY_BUILD;
+        this.delimChangeIndex = 0;
+        this.doParse(c);
+      }
+      break;
+    case DELIMITER_CHANGING:
+      if (!Character.isWhitespace(c))
+        this.parseStack.peek().append(c);
+      else {
+        this.delimiter = this.parseStack.pop().toString();
+        this.state = State.QUERY_BUILD;
+      }
+      break;
+    }
+  }
+
+  private void delimiter(CharBuffer buffer) {
+  }
+
+  public void execute() {
+    StringBuilder queryBuilder = new StringBuilder();
+    this.parseStack.push(queryBuilder);
+    CharBuffer buffer = CharBuffer.allocate(256);
+    try {
+      this.in.read(buffer);
+    } catch (IOException e) {
+      return;
+    }
+    buffer.flip();
+    while (buffer.hasRemaining()) {
+      this.doParse(buffer.get());
+    }
+  }
+}
